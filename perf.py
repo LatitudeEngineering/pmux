7131b6d3f2b0cc4e816f3f8f71eb5c646fb15571
Travis <traviswoodrow@email.arizona.edu>
2015-11-13 00:13:57 -0700

    Adds nanomsg and nnpy implementations

--- perf.py
+++ perf.py
@@ -1,12 +1,18 @@
 import zmq
 import time
 from multiprocessing import Process, Pipe
+from nanomsg import Socket, PAIR
+import nnpy
 
 def test_str(n):
     _str = ""
     for i in range(n):
         _str += "a"
     return _str
+
+
+SIZE = 2 ** 20
+
 
 #################################
 ##  ZeroMQ
@@ -20,6 +26,8 @@ def zmq_server(endpoint):
         while True:
             s.recv_string()
             s.send_string('ok')
+    except:
+        pass
     finally:
         s.close()
         ctx.destroy()
@@ -32,7 +40,7 @@ def zmq_client(endpoint):
     #s.connect('tcp://127.0.0.1:9001')
     count = 0
 
-    payload = test_str(2 ** 1)
+    payload = test_str(SIZE)
 
     start = time.time()
     try:
@@ -40,20 +48,108 @@ def zmq_client(endpoint):
             s.send_string(payload)
             s.recv_string()
             count += 1
+    except:
+        pass
     finally:
         end = time.time()
         total = end - start
         print 'total: ', count
         print 'took: ', total
         print 'req / sec:', count / total
+        print 'bandwidth: %f MBps' % (((count / total) * SIZE) / 2 ** 20)
         s.close()
         ctx.destroy()
 
 
+#################################
+##  Nanomsg
+#################################
+def nano_server(endpoint):
+    s = Socket(PAIR)
+    s.bind('ipc://' + endpoint)
+    #s.bind('tcp://127.0.0.1:9001')
+    try:
+        while True:
+            s.recv()
+            s.send('ok')
+    finally:
+        s.close()
+
+
+def nano_client(endpoint):
+    s = Socket(PAIR)
+    s.connect('ipc://' + endpoint)
+    #s.connect('tcp://127.0.0.1:9001')
+    count = 0
+
+    payload = test_str(SIZE)
+
+    start = time.time()
+    try:
+        while True:
+            s.send(payload)
+            s.recv()
+            count += 1
+    except:
+        pass
+    finally:
+        end = time.time()
+        total = end - start
+        print 'total: ', count
+        print 'took: ', total
+        print 'req / sec:', count / total
+        print 'bandwidth: %f MBps' % (((count / total) * SIZE) / 2 ** 20)
+        s.close()
+
+
+#################################
+##  nnpy
+#################################
+def nn_server(endpoint):
+    s = nnpy.Socket(nnpy.AF_SP, nnpy.PAIR)
+    s.bind('ipc://' + endpoint)
+    #s.bind('tcp://127.0.0.1:9001')
+    try:
+        while True:
+            s.recv()
+            s.send('ok')
+    finally:
+        s.close()
+
+
+def nn_client(endpoint):
+    s = nnpy.Socket(nnpy.AF_SP, nnpy.PAIR)
+    s.connect('ipc://' + endpoint)
+    #s.connect('tcp://127.0.0.1:9001')
+    count = 0
+
+    payload = test_str(SIZE)
+
+    start = time.time()
+    try:
+        while True:
+            s.send(payload)
+            s.recv()
+            count += 1
+    except:
+        pass
+    finally:
+        end = time.time()
+        total = end - start
+        print 'total: ', count
+        print 'took: ', total
+        print 'req / sec:', count / total
+        print 'bandwidth: %f MBps' % (((count / total) * SIZE) / 2 ** 20)
+        s.close()
+
+
+#################################
+##  multiprocessing
+#################################
 def mp_test():
 
     def client(conn):
-        payload = test_str(2 ** 1)
+        payload = test_str(SIZE)
         count = 0
         start = time.time()
         try:
@@ -67,6 +163,7 @@ def mp_test():
             print 'total: ', count
             print 'took: ', total
             print 'req / sec:', count / total
+            print 'bandwidth: %f MBps' % (((count / total) * SIZE) / 2 ** 20)
             conn.close()
 
     def server(conn):
